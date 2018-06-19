e3336c998d0e92e6761d75a55a0aac88bc80373a
Rafael Torres <rtorresz@gmail.com>
2018-06-19 09:32:06 -0500

    Modifing flyway conf6

--- visualaim_cms_full_demo/flyway.cmd
+++ visualaim_cms_full_demo/flyway.cmd
@@ -1,1 +1,1 @@
-/var/lib/jenkins/workspace/Flyway_DB_Evonik_training/flyway.cmd -configFile=/var/lib/jenkins/workspace/Flyway_DB_Evonik_training/visualaim_cms_full_demo/flyway_conf/flyway.conf $1
+../flyway/flyway -configFiles=./flyway_conf/flyway.conf $1
